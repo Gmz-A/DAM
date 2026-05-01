5da2064fda3813b2da79dd95ccb54df6e1e62224
Gmz-A <aitorg21@gmail.com>
2026-05-01 18:10:40 +0200

    Finalizacion Ejercicios Strings - 01/05/2026

--- Programacion/SES10_EstructuraDatos/SES10_EjerciciosString.java
+++ Programacion/SES10_EstructuraDatos/SES10_EjerciciosString.java
@@ -4,6 +4,9 @@ public class SES10_EjerciciosString {
 
     static Scanner sc = new Scanner(System.in);
     static String palabra;
+    static String palabra2;
+    static char[] vocales = {'a','e','i','o','u'};
+
 
     public static void main(String[] args) {
         selector();
@@ -27,33 +30,111 @@ public class SES10_EjerciciosString {
     }
 
     public static void ejercicio3(String valores) {
-    
+        
         valores = valores.toLowerCase();
+        int contadorVocales = 0;
 
         for (int i = 0; i < valores.length(); i++) {
-            char carac = valores.charAt(i);
-
+            for (int j = 0; j < vocales.length; j++) {
+                if ((valores.charAt(i))==(vocales[j])) {
+                    //System.out.println(valores.charAt(i));
+                    contadorVocales++;
+                }
+            }
         }
 
+        System.out.println("Esta palabra tiene "+contadorVocales+" vocales en ella :)");
     }
 
     public static void ejercicio4(String valores) {
         
-        
+        Boolean comprobacion = true;
 
+        System.out.print("Dame otra palabra -> ");
+        palabra2 = sc.next();
+
+        if (valores.length()==palabra2.length()) {
+            for (int i = 0; i < valores.length(); i++) {
+                while (comprobacion != false && i!=valores.length()) {
+                    if ((valores.charAt(i)==(palabra2.charAt(i)))) {
+                        comprobacion = true;
+                        i++;
+                    }else {
+                        comprobacion = false;
+                    }
+                }
+            }
+        } else {
+            comprobacion = false;
+        }
+
+        if (comprobacion==true) {
+            System.out.println("Equals -> Si son iguales :)");
+        } else {
+            System.out.println("Equals -> Son palabras diferentes :(");
+        }
     }
 
     public static String ejercicio5(String frase) {
         
         StringBuilder reversa = new StringBuilder();
 
-        solicitarPalabra();
-
         for (int i = frase.length()-1; i >= 0; i--) {
             reversa.append(frase.charAt(i));
         }
         
         return reversa.toString();
+    }
+
+    public static void ejercicio6(String valores) {
+        
+        int contador = 0;
+
+        System.out.print("Dime una letra que buscar -> ");
+        String letra = sc.next();
+
+        char letraF = letra.charAt(0);
+
+        for (int i = 0; i < valores.length(); i++) {
+            if (valores.charAt(i)==letraF) {
+                contador++;
+            }
+        }
+
+        System.out.println("La palabra letra elegida a buscar es -> "+letraF);
+        System.out.println("Esa letra sale "+contador+" veces :)");
+    }
+
+    public static void ejercicio7(String valores) {
+        
+        valores = valores.trim();
+        char sepacaion = ' ';
+        int espacio = 0;
+        int espacio2= 0;
+
+        for (int i = 0; i < valores.length(); i++) {
+            if (valores.charAt(i)==sepacaion) {
+                espacio = i;
+                System.out.println(valores.substring(espacio2, espacio));
+                espacio2 = espacio+1;
+            } else {
+                espacio = valores.length();
+            }
+        }
+        System.out.println(valores.substring(espacio2,espacio));
+    }
+
+    public static void ejercicio8() {
+        
+        String texto = " hola mundo ";
+
+        texto = texto.trim();
+
+        texto = texto.replace("mundo", "java");
+
+        texto = texto.toUpperCase();
+
+        System.out.println(texto);
     }
 
     public static boolean ejercicio9(String frase) {
@@ -71,11 +152,102 @@ public class SES10_EjerciciosString {
         }
     }
 
+    public static void ejercicio10(String valores) {
+
+        valores = valores.trim();
+
+        valores = valores.replace(' ', ',');
+
+        String[] palabras = valores.split(",");
+        System.out.println("La frase tiene "+palabras.length+" palabras");
+
+    }
+
+    public static void ejercicio11(String valores) {
+        
+        valores = valores.trim();
+        valores = valores.toLowerCase();
+        String frasefinal = " ";
+
+        for (int i = 0; i < valores.length(); i++) {
+            for (int j = 0; j < vocales.length; j++) {
+                if (valores.charAt(i)==vocales[j]) {
+                    frasefinal += '*';
+                    i++;
+                }
+            }
+            frasefinal += valores.charAt(i);
+        }
+        frasefinal = frasefinal.trim();
+        System.out.println(frasefinal);
+    }
+
+    public static void ejercicio12(String valores) {
+        
+        boolean comprobaciones = false;
+        String caracterEspecial = "@#$%&*{}";
+
+        if (valores.length()<=8) {
+            comprobaciones = false;
+        } else {
+            comprobaciones = true;
+        }
+
+        /*if (valores.toLowerCase()==valores.toUpperCase()) {
+            comprobaciones = false;
+        } else if (comprobaciones != false) {
+            comprobaciones = true;
+        }*/
+        String palabra = valores.toUpperCase();
+
+        if (comprobaciones != false) {
+            for (int i = 0; i < valores.length(); i++) {
+                if (valores.charAt(i)==palabra.charAt(i)) {
+                    for (int j = 0; j < vocales.length; j++) {
+                        if (comprobaciones =! false) {
+                            if (valores.charAt(i)==caracterEspecial.charAt(j)) {
+                                comprobaciones = false;
+                            } else if (comprobaciones != false) {
+                                comprobaciones = true;
+                            }
+                        }
+                    }
+                } 
+            }
+        }
+
+        if (comprobaciones != false) {
+            for (int i = 0; i < valores.length(); i++) {
+                for (int j = 0; j < caracterEspecial.length(); j++) {
+                    if (valores.charAt(i)==caracterEspecial.charAt(j)) {
+                        if(comprobaciones =! false){
+                            comprobaciones = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (comprobaciones==true) {
+            System.out.println("Contraseña Segura :)");
+        } else {
+            System.out.println("Contraseña no segura :(");
+        }
+    }
 
     public static String solicitarPalabra() {
         
         System.out.print("Dime una palabra -> ");
         palabra = sc.next();
+        
+        return palabra;
+    }
+
+    public static String solicitarFrase() {
+        
+        sc.nextLine();
+        System.out.print("Escribe una pequeña frase -> ");
+        palabra = sc.nextLine();
         
         return palabra;
     }
@@ -86,7 +258,7 @@ public class SES10_EjerciciosString {
 
         while (ejercicio != 0) {
             
-            System.out.print("Que ejercicio quieres ver (0 para finalizar) -> ");
+            System.out.print("\nQue ejercicio quieres ver (0 para finalizar) -> ");
             ejercicio = sc.nextInt();
 
             switch (ejercicio) {
@@ -99,22 +271,27 @@ public class SES10_EjerciciosString {
                     ejercicio2(palabra);
                     break;
                 case 3:
-
+                    solicitarPalabra();
+                    ejercicio3(palabra);
                     break;
                 case 4:
-
+                    solicitarPalabra();
+                    ejercicio4(palabra);
                     break;
                 case 5:
+                    solicitarPalabra();
                     System.out.println(ejercicio5(palabra));
                     break;
                 case 6:
-
+                    solicitarPalabra();
+                    ejercicio6(palabra);
                     break;
                 case 7:
-
+                    solicitarFrase();
+                    ejercicio7(palabra);
                     break;
                 case 8:
-
+                    ejercicio8();
                     break;
                 case 9:
                     solicitarPalabra();
@@ -125,18 +302,23 @@ public class SES10_EjerciciosString {
                     }
                     break;
                 case 10:
-
+                    solicitarFrase();
+                    ejercicio10(palabra);
                     break;
                 case 11:
-
+                    solicitarPalabra();
+                    ejercicio11(palabra);
                     break;
                 case 12:
-
+                    solicitarPalabra();
+                    ejercicio12(palabra);
                     break;        
                 default:
                     break;
             }
         }
-        System.out.println("Gracias por interactuar :)");
+        System.out.println("\n:) :) :) :) :) :) :) :) :) :)");
+        System.out.println(":) Gracias por interactuar :)");
+        System.out.println(":) :) :) :) :) :) :) :) :) :)");
     }
 }
